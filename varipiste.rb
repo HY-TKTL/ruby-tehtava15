3145e8653d219748b29440a8039450af586e0d4f
Lensuman <jevgeni.lensu@gmail.com>
2016-01-23 17:00:35 +0200

    Update varipiste.rb

--- varipiste.rb
+++ varipiste.rb
@@ -1,13 +1,12 @@
 require './piste.rb'
 
 class Varipiste < Piste
- def initialize(x,y, vari)
-  super(x,y)
-  @vari = vari
+ def initialize (x, y, vari)
+   super(x, y)
+   @vari = vari
  end
-
+ 
  def to_s
-  "(#{@x}, #{@y}) #{@vari}"
-	
+   super.to_s + " #{@vari}"
  end
-end
+end 
